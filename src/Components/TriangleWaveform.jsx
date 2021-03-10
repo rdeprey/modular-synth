a16fd2defe1eb389cec672b96b6de9dfdcb3cac6
Julie Bodian <julia.bodian@vistaprint.com>
2021-03-10 15:00:09 -0500

    Add button and triangle waveform component

---
+++ src/Components/TriangleWaveform.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TriangleWaveform = () => {
+    return <svg role="img" aria-labelledby="triangle-waveform" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="640" height="320">
+        <title id="triangle-waveform">Triangle Waveform</title>
+        <defs><path d="M640 132.83L507.17 326.04 132.83 0 0 188.76" id="a"/></defs><use xlinkHref="#a" fill-opacity="0" stroke="#000"/></svg>
+}
+
+export default TriangleWaveform;
