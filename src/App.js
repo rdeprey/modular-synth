8c01dfd34344b4e4e308b2244155ad29102f6e86
Julie Bodian <julia.bodian@vistaprint.com>
2021-03-29 15:17:02 -0400

    Add square wave that looks like triangle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TriangleWaveform from './Components/TriangleWaveform';
+import SquareWaveform from './Components/SquareWaveform';
 import { Button } from './Components/Button';
 
 function App() {
   return (
     <div className="App">
       <Button><TriangleWaveform /></Button>
+      <Button><SquareWaveform /></Button>
     </div>
   );
 }
---
+++ src/Components/SquareWaveform.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SquareWaveform = () => {
+    return <svg role="img" aria-labelledby="square-waveform" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="640" height="320">
+        <title id="square-waveform">Square Waveform</title>
+        <defs><path d="M634.77 160v160H532.65V0H430.53v320H326.01V0H215.48v320H102.54V0H6.43v160" id="a"/></defs>
+        <use xlinkHref="#a" fill-opacity="0" stroke="#000"/></svg>
+}
+
+export default SquareWaveform;
+
